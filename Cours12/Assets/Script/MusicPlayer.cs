7496b7a24cb23033bca255d5e985b3e7217abdd9
B533P-15\533 <tristan.fecteau@effecto.ca>
2019-09-30 15:36:01 -0400

    travail fini

---
+++ Cours12/Assets/Script/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour{
+
+    public void Start()
+    {
+        int numberOfMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+        if(numberOfMusicPlayer > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Cours12/Assets/Script/TriggerCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCollision : MonoBehaviour {
+	public void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            FindObjectOfType<AudioSource>().Play();
+        }
+    }
+}
